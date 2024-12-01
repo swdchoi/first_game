23a24fca169190fa70a0348fd0002f9911651dcf
swdchoi <sungwonchoi224@gmail.com>
2024-12-01 23:29:09 +0800

    added another enemy

--- script.js
+++ script.js
@@ -10,18 +10,25 @@ let playerXposition = 200;
 let playerYposition = 0;
 let enbot = 500;
 let enleft = 500;
+let enbot1 = 500;
+let enleft1 = 500;
 
 
 
 const playerLost = () => {
 const enemyMan = document.getElementById("enemy")
+const enemyMan1 = document.getElementById("enemy1");
     if(player.style.bottom == enemyMan.style.bottom && player.style.left == enemyMan.style.left)
     {
         alert("GAME END!")
+    } else if (player.style.bottom == enemyMan1.style.bottom && player.style.left == enemyMan1.style.left) {
+        alert("endgame")
     }
 }
  
 const gridMaker = () => {
+   
+
     for(let i = 0; i <= 35; i++){
         let grid = document.createElement("div");
         grid.style.height = "100px";
@@ -45,6 +52,18 @@ const enemyMaker = (obj) =>{
     canv.appendChild(em);
 }
 
+const enemyMaker2 = (obj) =>{
+    const em = document.createElement("div");
+    em.id = "enemy1";
+    em.style.height = obj.height;
+    em.style.width = obj.width;
+    em.style.backgroundColor = "green";
+    em.style.position = "absolute";
+    em.style.bottom = obj.positionY;
+    em.style.left = obj.positionX;
+    canv.appendChild(em);
+}
+
 const enemy = {
     height: "100px",
     width: "100px",
@@ -54,8 +73,33 @@ const enemy = {
 }
 
 const thisenemyMovement = () => {
-    const enemyMan = document.getElementById("enemy");
+    const enemyMan1 = document.getElementById("enemy1");
 
+    if (enbot1 <= 550 && enleft1 >= 0 && enleft1 <= 500 && enbot1 >= 0){
+        const outof41 = Math.random()*40
+        if(outof41 <= 10) {
+            enbot1 -= leftWalkSpeed;
+        } else if (outof41 > 10 && outof41 <= 20) {
+            enbot1 += rightWalkSpeed;
+        } else if (outof41 > 20 && outof41 <= 30) {
+            enleft1 += forwardWalkSpeed
+        } else if (outof41 > 30 && outof41 <= 40) {
+            enleft1 -= backwardWalkSpeed;
+        }
+    } else if (enbot1 > 550) {
+        enbot1 -= leftWalkSpeed;
+    } else if (enbot1 < 0){
+        enbot1 += rightWalkSpeed;
+    } else if (enleft1 < 0){
+        enleft1 += forwardWalkSpeed;
+    } else if (enleft1 > 500) {
+        enleft1 -= backwardWalkSpeed;
+    }
+
+    enemyMan1.style.bottom = `${enbot1}px`;
+    enemyMan1.style.left = `${enleft1}px`;
+
+    const enemyMan = document.getElementById("enemy");
 
     if (enbot <= 550 && enleft >= 0 && enleft <= 500 && enbot >= 0){
         const outof4 = Math.random()*40
@@ -78,11 +122,9 @@ const thisenemyMovement = () => {
         enleft -= backwardWalkSpeed;
     }
 
-    console.log(enleft);
-    console.log(enbot);
-
     enemyMan.style.bottom = `${enbot}px`;
     enemyMan.style.left = `${enleft}px`;
+
 }
 
 
@@ -122,5 +164,6 @@ document.addEventListener("keypress", (e) => {
 document.addEventListener("DOMContentLoaded", () => 
 {   gridMaker();
     enemyMaker(enemy);
+    enemyMaker2(enemy);
 })
 
